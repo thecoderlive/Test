ae044f36191e3ccc4bce6fbe4bf2582ab0957177
thecoder.live <82015917+thecoderlive@users.noreply.github.com>
2023-03-29 14:55:57 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "0a90a6a0-d4ab-498e-90bc-121f3a78f17e",
+    "home_feed": [
+        {
+            "id": "fadd823d-3b3e-4f96-9544-a0913f9b5498",
+            "artist_name": "month",
+            "home_carousel": [
+                {
+                    "id": "a3561759-9aa1-43ce-8f37-c4cf4a8d6fbf",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/258.png"
+                },
+                {
+                    "id": "43c8a50a-adad-45be-bba8-f9681e18d07d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/57.png"
+                },
+                {
+                    "id": "072f24f2-9cc7-42ce-b8c2-8edf7426da36",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/51.png"
+                },
+                {
+                    "id": "c996c79a-e00e-4224-8f9b-90c982ffa187",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/208.png"
+                },
+                {
+                    "id": "2467754c-5f1b-4252-bf8f-341f988b69bc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/249.png"
+                }
+            ],
+            "art_caption": "answer",
+            "art_description": "Tv imagine operation him young. Which outside most character water."
+        },
+        {
+            "id": "bfa3fe6b-7ad5-4a28-8e0a-e291a824273e",
+            "artist_name": "perhaps",
+            "home_carousel": [
+                {
+                    "id": "931c15a5-34ab-4de1-81e0-7de5f81679a6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/82.png"
+                },
+                {
+                    "id": "24546464-9668-4074-93df-5f4983ef0915",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/325.png"
+                },
+                {
+                    "id": "dbce4802-e6d8-4bfc-b1e7-2b6c09c5e278",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/20.png"
+                },
+                {
+                    "id": "a9d5677f-d918-47fa-9959-a0f6c4ed2a08",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png"
+                },
+                {
+                    "id": "fe5a1317-38f6-4bc4-8c4c-dd9f36613053",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/357.png"
+                }
+            ],
+            "art_caption": "though",
+            "art_description": "Lot popular interview data anything either subject. Record interesting behavior her skin. Space nor country opportunity. Win close report hair act fall."
+        },
+        {
+            "id": "0d3162dd-6077-444b-908b-be114407b758",
+            "artist_name": "game",
+            "home_carousel": [
+                {
+                    "id": "d9d2bcd4-dab6-4a46-94d2-36de51c99549",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/312.png"
+                },
+                {
+                    "id": "31f07a13-809f-499a-bb7f-74b2848ead36",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/249.png"
+                },
+                {
+                    "id": "7aa47fb2-14ad-414d-b562-84e664d80a1a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/90.png"
+                },
+                {
+                    "id": "c5c95a5a-dff2-436c-9105-7079ecde5625",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/241.png"
+                },
+                {
+                    "id": "9c2ccfdf-080e-4e7c-9a14-8ea09ac7f6ac",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/305.png"
+                }
+            ],
+            "art_caption": "successful",
+            "art_description": "Treat because help state per. Leg commercial level. Never expect black special break under."
+        },
+        {
+            "id": "422c0ba3-8ad9-4939-a539-84ba37ebecbc",
+            "artist_name": "truth",
+            "home_carousel": [
+                {
+                    "id": "56f4c77f-3d13-4786-8056-0e06fddfad4a",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/253.png"
+                },
+                {
+                    "id": "fecf96a5-95ba-4c24-89ce-5e8f286fdff5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/362.png"
+                },
+                {
+                    "id": "9f044f3b-9219-49d5-8ab9-4e69e5598e9f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/231.png"
+                },
+                {
+                    "id": "55f58ff0-fe02-410d-b1a3-d85f87cafb9e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/348.png"
+                },
+                {
+                    "id": "091c58a7-0c85-4048-9045-1e2b8de0ac60",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/43.png"
+                }
+            ],
+            "art_caption": "most",
+            "art_description": "Five father security leader. Investment off agent certainly full statement people. Way cover music of resource trial."
+        },
+        {
+            "id": "87be8a51-c1f1-4682-bd47-92390c4f03b8",
+            "artist_name": "laugh",
+            "home_carousel": [
+                {
+                    "id": "f998e734-7896-498e-88db-5aca4dfdef60",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/140.png"
+                },
+                {
+                    "id": "aa63b8b7-e812-464a-a790-1e1543b8c166",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/135.png"
+                },
+                {
+                    "id": "11112cf5-567b-475d-8bee-61079edf3720",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/282.png"
+                },
+                {
+                    "id": "72a13358-be67-47d0-956e-20785e7bf71b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/244.png"
+                },
+                {
+                    "id": "9062a08b-2c51-4a2f-a0bd-04921c6887bb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/346.png"
+                }
+            ],
+            "art_caption": "off",
+            "art_description": "Relate collection same participant."
+        }
+    ]
+}
